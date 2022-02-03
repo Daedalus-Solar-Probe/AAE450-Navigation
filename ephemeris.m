493047260119e9a8ee312f9b44d3eac436215310
Matt Mader <mattmaderCT@gmail.com>
2022-02-03 16:24:57 -0500

    IR of emphemeris model

--- ephemeris.m
+++ ephemeris.m
@@ -1,11 +1,41 @@
-%% Clear
-clear
-clc
-
-%% Setup
+function [x_ecl,y_ecl,z_ecl,x_icrf,y_icrf,z_icrf] = ephemeris(T_eph)
+% EPHEMERIS Provides cartesian positions of all 8 planets relative to the
+% sun in either J2000 Ecliptic or ICRF reference frames. Implemented to be
+% vectorized as much as possible and therefore very fast as compared to the
+% MATLAB Aerospace Dataset function "planetEphemeris" but is less accurate.
+%
+% Inputs:
+%   T_eph: row vector containing ephemeris times to be evaluated at in
+%       Julian Days (see MATLAB Aerosapce toolbox function "juliandate"
+%
+% Outputs:
+%   x_ecl: 8x(len(T_eph)) containing the series of x coordinate location
+%       relative to the J2000 Mean Ecliptic Frame for planets in
+%       increasing (Mercury, Venus, Earth, ...) order in AU.
+%
+%   y_ecl: ""
+%
+%   z_ecl: ""
+%
+%   x_icrf: 8x(len(T_eph)) containing the series of x coordinate location
+%       relative to the J2000 ICRF frame for planets in increasing 
+%       (Mercury, Venus, Earth, ...) order in AU.
+%
+% Information:
+%   Author: Matthew Mader
+%   Contact: maderm@purdue.edu
+%   Date: Feb 3, 2022
+%
+% Revision History:
+%   Rev: IR
+%   Notes: Initial release, conducted limited tests compared against to the
+%       "planetEphemeris" function in ICRF for inner planets.
+%
+% Data Source:
+%   https://ssd.jpl.nasa.gov/planets/approx_pos.html
 
 % objects
-objs = [
+objects = [
     "Mercury"
     "Venus"
     "EM Bary"
@@ -17,7 +47,7 @@ objs = [
     ];
 
 % semi-major axis [au]
-a = [
+a0 = [
     0.38709927
     0.72333566
     1.00000261
@@ -41,7 +71,7 @@ a_dot = [
     ];
 
 % eccentricity [1]
-e = [
+e0 = [
     0.20563593
     0.00677672
     0.01671123
@@ -64,8 +94,8 @@ e_dot = [
      0.00005105
     ];
 
-% inclination [deg]
-I = [
+% inclination [rad]
+I0 = deg2rad([
      7.00497902
      3.39467605
     -0.00001531
@@ -74,10 +104,10 @@ I = [
      2.48599187
      0.77263783
      1.77004347
-    ];
+    ]);
 
-% inclination rate [deg/Cy]
-I_dot = [
+% inclination rate [rad/Cy]
+I_dot = deg2rad([
     -0.00594749
     -0.00078890
     -0.01294668
@@ -86,10 +116,10 @@ I_dot = [
      0.00193609
     -0.00242939
      0.00035372
-    ];
+    ]);
 
-% mean longitude [deg]
-L = [
+% mean longitude [rad]
+L0 = deg2rad([
     252.25032350
     181.97909950
     100.46457166
@@ -98,10 +128,10 @@ L = [
      49.95424423
     313.23810451
     -55.12002969
-    ];
+    ]);
 
-% mean longitude rate [deg/Cy]
-L_dot = [
+% mean longitude rate [rad/Cy]
+L_dot = deg2rad([
     149472.67411175
      58517.81538729
      35999.37244981
@@ -110,10 +140,10 @@ L_dot = [
       1222.49362201
        428.48202785
        218.45945325
-    ];
+    ]);
 
-% longitude of perihelion [deg]
-long_peri = [
+% longitude of perihelion [rad]
+long_peri0 = deg2rad([
      77.45779628
     131.60246718
     102.93768193
@@ -122,10 +152,10 @@ long_peri = [
      92.59887831
     170.95427630
      44.96476227
-    ];
+    ]);
 
-% longitude of perihelion rate [deg/Cy]
-long_peri_dot = [
+% longitude of perihelion rate [rad/Cy]
+long_peri_dot = deg2rad([
      0.16047689
      0.00268329
      0.32327364
@@ -134,10 +164,10 @@ long_peri_dot = [
     -0.41897216
      0.40805281
     -0.32241464
-    ];
+    ]);
 
-% longitude of the ascending node [deg]
-long_node = [
+% longitude of the ascending node [rad]
+long_node0 = deg2rad([
      48.33076593
      76.67984255
       0.0
@@ -146,10 +176,10 @@ long_node = [
     113.66242448
      74.01692503
     131.78422574
-    ];
+    ]);
 
-% longitude of the ascending node rate [deg/Cy]
-long_node_dot = [
+% longitude of the ascending node rate [rad/Cy]
+long_node_dot = deg2rad([
     -0.12534081
     -0.27769418
      0.0
@@ -158,4 +188,117 @@ long_node_dot = [
     -0.28867794
      0.04240589
     -0.00508664
-    ];
+    ]);
+
+% additional terms for mean anomaly
+b = deg2rad([
+     0.0
+     0.0
+     0.0
+     0.0
+    -0.00012452
+     0.00025899
+     0.00058331
+    -0.00041348
+    ]);
+
+c = deg2rad([
+     0.0
+     0.0
+     0.0
+     0.0
+     0.06064060
+    -0.13434469
+    -0.97731848
+     0.68346318
+    ]);
+
+s = deg2rad([
+     0.0
+     0.0
+     0.0
+     0.0
+    -0.35635438
+     0.87320147
+     0.17689245
+    -0.10162547
+    ]);
+
+f = deg2rad([
+    0.0
+    0.0
+    0.0
+    0.0
+    38.35125000
+    38.35125000
+    7.67025000
+    7.67025000
+    ]);
+
+% centuries past J2000.0
+T = (T_eph - 2451545.0)/36525; % [cY]
+
+
+% current semi-major axis
+a = a0 + a_dot*T; % [au]
+
+% current eccentricity
+e = e0 + e_dot*T; % [1]
+
+% current incination
+I = I0 + I_dot*T; % [rad]
+
+% current mean longitude
+L = L0 + L_dot*T; % [rad]
+
+% current longitude of perihelion
+omega_bar = long_peri0 + long_peri_dot*T; % [rad]
+
+% current longitude of the ascending node
+OMEGA = long_node0 + long_node_dot*T; % [rad]
+
+
+% argument of perihelion
+omega = omega_bar - OMEGA; % [rad]
+
+% mean anomaly
+M = L - omega_bar + b*T.^2 + c.*cos(f*T) + s.*sin(f*T); % [rad]
+
+% mean anomaly can only be, -pi <= M <= pi
+M = mod(M+pi,2*pi) - pi; % [rad]
+
+
+% Newton-Raphson to find eccentric anomaly
+E = M; % [rad] initial guesses
+
+% iterate until convergence is reached
+tol = 1e-6;
+for i = 1:size(objects,1)
+    for j = 1:length(T_eph)
+        while abs(E(i,j)-M(i,j)-e(i,j)*sin(E(i,j))) > tol
+            E(i,j) = E(i,j)-(E(i,j)-M(i,j)-e(i,j)*sin(E(i,j)))/(1-e(i,j)*cos(E(i,j))); % [rad]
+        end
+    end
+end
+
+% heliocentric coordinates in orbit plane
+x_orbit = a.*(cos(E)-e); % [au]
+y_orbit = a.*sqrt(1-e.^2).*sin(E); % [au]
+
+% heliocentric coodinates in J2000 Ecliptic Plane
+x_ecl = (cos(omega).*cos(OMEGA)-sin(omega).*sin(OMEGA).*cos(I)).*x_orbit + ...
+    (-sin(omega).*cos(OMEGA)-cos(omega).*sin(OMEGA).*cos(I)).*y_orbit;
+
+y_ecl = (cos(omega).*sin(OMEGA)+sin(omega).*cos(OMEGA).*cos(I)).*x_orbit + ...
+    (-sin(omega).*sin(OMEGA)+cos(omega).*cos(OMEGA).*cos(I)).*y_orbit;
+
+z_ecl = sin(omega).*sin(I).*x_orbit + cos(omega).*sin(I).*y_orbit;
+
+% if user want the coordinates in J2000 ICRF
+if nargout > 3
+    % obliquity at J2000
+    epsilon = deg2rad(23.43928);
+    x_icrf = x_ecl;
+    y_icrf = cos(epsilon)*y_ecl - sin(epsilon)*z_ecl;
+    z_icrf = sin(epsilon)*y_ecl + cos(epsilon)*z_ecl;
+end
