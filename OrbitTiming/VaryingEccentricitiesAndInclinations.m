c08c4bc6f664a8f8b53d590f8cf2593de3a01595
Frank Latorre <franklatorre12345@gmail.com>
2022-04-02 04:28:02 -0400

    Franks Eccentricity Calculators

---
+++ OrbitTiming/VaryingEccentricitiesAndInclinations.m
@@ -0,0 +1,142 @@
+%This code is designed to take the desired semi-major axis, the desired
+%observation period, and the target latitude, and display the various observation
+%times that various eccentricities and inclinations would work
+clc;clf;clear
+
+%Inputs
+a=0.48;
+TargetLat=60;
+LightnessFactor=0.1;%In case you want to simulate the effect of pointing the sail at the sun. if not, just set to 0
+plotOptions=[1,1];
+myEccStep=0.01;
+myIncStep=0.01;
+TargetObservationPeriod=40; %days
+ConeVerticies=100;
+ShowCone=1;
+DisplayTable=1;
+
+%Givens & Initializations
+rSun=0.00465047; %AU
+uSun=(1-LightnessFactor)*3.9640*10^-14; %AU^3/s^2
+TargetLat=TargetLat*pi/180;%Convert to radians
+Theta=(0:0.01:2*pi)'; %vector of thetas
+Period=2*pi*sqrt(a^3/uSun)/24/3600; %Theta Vector
+ecc=0:myEccStep:0.9; %eccentricity Vector
+myIncValues=(0:myIncStep:pi/2); %Inclination Values
+myBest3DXs = zeros(length(myIncValues),length(Theta));
+myBest3DYs = zeros(length(myIncValues),length(Theta));
+myBest3DZs = zeros(length(myIncValues),length(Theta));
+myBestEccs = zeros(length(myIncValues),1);
+
+for i=1:length(myIncValues)
+    Inclination=myIncValues(i);
+    %Calculate Keplerian Parameters
+    P=a.*(1-ecc.^2);
+    r=(P./(1+ecc.*cos(Theta)))'; %Matrix of r values, where each row is a different eccentricity
+    x=r.*cos(Theta'); %2-D X positions of each orbit
+    y=r.*sin(Theta'); %2-D Y positions of each orbit
+    myIndices=find(Theta>=pi); %index at highest radius
+    myrP=r(:,1); %Perigee Values
+    myrAP=r(:,myIndices(1)); %Apogee Values
+    
+    %Rotate 2-D plane about Y axis
+    A=cos(Inclination); %First element of rotation matrix
+    B=sin(Inclination); %Other element of rotation matrix
+    my3DX=x.*A;
+    my3DY=y;
+    my3DZ=-x.*B;
+    
+    %Find points within the cone
+    % I noticed this is exactly the same for all orbits if they are in the
+    % same general orbit, but if you dont believe me uncomment 68 and 82
+    
+    OrbitNum=1; BinaryMatrix=0*my3DX; %Binary blank matrix, shows where intersection points are
+    for OrbitNum = 1:length(ecc)
+        ThisX=my3DX(OrbitNum,:); %this orbits X values
+        ThisY=my3DY(OrbitNum,:); %This orbits Y values
+        ThisZ=my3DZ(OrbitNum,:); %This orbits Z values
+        ThisAxisR=sqrt(ThisX.^2 + ThisY.^2); %Radi of the points from the z-axis
+        myRatio=tan(TargetLat); %ratio for the cone: z/x
+        for myPoint=1:length(ThisX) %loops through every point in the orbit
+            thisConeR=ThisZ(myPoint)/myRatio; %Radius of the cone from the current zaxis
+            if (thisConeR>ThisAxisR(myPoint)) %if the point is within the cone
+                BinaryMatrix(OrbitNum,myPoint) = 1;
+            end
+        end
+    end
+    
+    if ~sum(sum(BinaryMatrix))
+        %invalid inclination
+    else
+        %Isolate Vectors and rotate them back to 2-D
+        BinaryVec=BinaryMatrix(1,:); %due to geometry, all of these vectors are the same, so only need first
+        VecIndicies=find(BinaryVec==1); %indices where the craft is within the observation period
+        ObservationVecs=[my3DX(:,VecIndicies(1)) my3DY(:,VecIndicies(1)) my3DZ(:,VecIndicies(1))]; %3-D Vectors where observation can begin for each orbit
+        ToFlatRotationVec=inv([A 0 B; 0 1 0; -B 0 A]); %Rotation vector to convert 3-D back to 2-D
+        myNewXY=ToFlatRotationVec*ObservationVecs'; %2-D vectors of starting observation point
+        newX=myNewXY(1,:);
+        newY=myNewXY(2,:);
+        
+        %Identify Observation Time
+        newTheta=pi+atan(newY./newX); %theta at starting observation point
+        TargetBigE=2*atan( sqrt((1-ecc)./(1+ecc)) .*tan(newTheta/2)); %Eccentric anomaly
+        TargetMe=TargetBigE-ecc.*sin(TargetBigE); %Mean Anomaly
+        tDays=(Period - 2*( abs( TargetMe*Period/(2*pi) ) ) ); %Observation time in days
+        
+        ValidIndices=find(tDays>TargetObservationPeriod);
+        if sum(ValidIndices)
+            myBest3DXs(i,:) = my3DX(ValidIndices(1),:);
+            myBest3DYs(i,:) = my3DY(ValidIndices(1),:);
+            myBest3DZs(i,:) = my3DZ(ValidIndices(1),:);
+            myBestEccs(i) = ecc(ValidIndices(1));
+        end
+        
+    end
+end
+
+
+
+
+if plotOptions(1)
+    clf(1)
+    figure (1)
+    plot(myIncValues*180/pi, myBestEccs); grid on;
+    xlabel("inclination"); ylabel("min eccentricity required")
+end
+if plotOptions(2)
+    Temp=sum(plotOptions(1:2));
+    clf(Temp)
+    figure (Temp)
+    for J=1:length(myIncValues)
+        plot3( myBest3DXs(J,:),myBest3DYs(J,:),myBest3DZs(J,:)); hold on;
+    end
+    %Plot Sun
+    [SunX SunY SunZ]=sphere(10); SunX=SunX.*rSun; SunY=SunY.*rSun; SunZ=SunZ.*rSun;
+    surf(SunX, SunY, SunZ);
+    
+    if ShowCone
+        %Plot Target Cone:
+        rConeVec = linspace(0,max(max(myBest3DZs))/tan(TargetLat),ConeVerticies) ;
+        th = linspace(0,2*pi,ConeVerticies);
+        [RCone,T] = meshgrid(rConeVec,th) ;
+        XCone = RCone.*cos(T) ;
+        YCone = RCone.*sin(T) ;
+        ZCone = sqrt(XCone.^2 + YCone.^2)/cos(TargetLat);
+        hold on; surf(XCone,YCone,ZCone)
+    end
+    
+    daspect([1 1 1]);
+    pbaspect([1 1 1]); grid on;
+    xlabel("X"); ylabel("Y"); zlabel("Z")
+end
+
+%TRUNCATE
+GoodVals=find(myBestEccs~=0);
+myBest3DXs=myBest3DXs(GoodVals,:); myBest3DYs=myBest3DYs(GoodVals,:); myBest3DZs=myBest3DZs(GoodVals,:);
+myR=sqrt(myBest3DXs.^2 + myBest3DYs.^2 + myBest3DZs.^2);
+myBestEccs=myBestEccs(GoodVals,:); myIncValues=myIncValues(GoodVals); 
+if DisplayTable
+    fprintf("At a semi major axis of %.3f AU an orbit will take %.2f days. In order to reach the %.0f day observation period: \n",a,Period,TargetObservationPeriod)
+    eccentricityValues=myBestEccs; inclination=myIncValues'*180/pi; Apogees=max(myR')'; Perigees=min(myR')';
+    disp(table(eccentricityValues, inclination, Apogees, Perigees))
+end
